25a66cfe31864017a37588417fc1a269360f30ad
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-08-22 18:40:53 +0600

    filter UI

--- app/(pages)/chat/_components/Hero.tsx
+++ app/(pages)/chat/_components/Hero.tsx
@@ -16,20 +16,21 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "../../../components/Generating";
 import Notification from "../../../components/Notification";
-
+import ProfessorReview from "./ProfessorReview";
 const Hero = () => {
   const parallaxRef = useRef(null);
 
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25rem]"
+      className="pt-[8rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
       id="hero"
     >
-      <div className="chatpage max-w-[1240px] mx-auto px-4">
-        <h1>chat content</h1>
+      <div className="chatpage max-w-[1240px] mx-auto px-4 pb-4 flex flex-col gap-4">
+        <h1>Filter</h1>
+        <ProfessorReview/>
       </div>
       <BottomLine />
     </Section>
